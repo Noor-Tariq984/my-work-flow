51ea1c9c7e86aa1b0e7ca98c6ecb64af9e5c4905
Noor-Tariq984 <noortariqhussain@gmail.com>
2026-08-13 06:55:13 -0700

    Delete state.js

--- state.js
+++
@@ -1,7 +0,0 @@
-import {loadState, saveState} from "./storage.js";
-export class TaskStore {
-  constructor(){ this.state=loadState(); this.listeners=new Set(); }
-  get tasks(){ return [...this.state.tasks].sort((a,b)=>a.order-b.order); }
-  replaceTasks(tasks){ this.state={...this.state,tasks:tasks.map((t,i)=>({...t,order:i}))}; saveState(this.state); this.listeners.forEach(fn=>fn(this.tasks)); }
-  subscribe(fn){this.listeners.add(fn); fn(this.tasks); return ()=>this.listeners.delete(fn)}
-}
